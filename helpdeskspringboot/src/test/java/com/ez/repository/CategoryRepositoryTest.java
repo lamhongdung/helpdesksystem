9bc564803d673cae7e54f09b9fc405ebb509de16
lamhongdung <dunglh@gmail.com>
2023-05-15 17:04:16 +0700

    junit test - categoryRepository

--- helpdeskspringboot/src/test/java/com/ez/repository/CategoryRepositoryTest.java
+++ helpdeskspringboot/src/test/java/com/ez/repository/CategoryRepositoryTest.java
@@ -1,7 +1,7 @@
 package com.ez.repository;
 
 import com.ez.entity.Category;
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,6 +15,8 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.jdbc.JdbcTestUtils;
+
+import java.util.List;
 
 //@ExtendWith(SpringExtension.class)
 @DataJpaTest
@@ -31,33 +33,96 @@ public class CategoryRepositoryTest {
     private JdbcTemplate jdbcTemplate;
     Category category;
 
+    // @BeforeEach: run this method setup() before each test method
     @BeforeEach
     public void setup() {
 
-        // delete all data
+        // delete all data in tables:
+        // comment, ticket, fileStorage, teamSupporter, team,
+        // priority, category, user.
         JdbcTestUtils.deleteFromTables(jdbcTemplate,
                 "comment", "ticket", "fileStorage",
                 "teamSupporter", "team", "priority",
                 "category", "user");
 
         // create new category
-        category = new Category("unit test - category 1", "Active");
+        category = new Category("JUnit test - category 1", "Active");
 
     }
 
-    //
-    @DisplayName("Unit test for creating new category")
+    // JUnit test for creating new category
+    @DisplayName("JUnit test for creating new category")
     @Test
 //    @Rollback(value = false)
-    public void whenCreateNewCategory_thenReturnIdGreater0() {
+    public void whenCreateNewCategory_thenReturnIdGreaterZero() {
 
         // save new category into database
         Category savedCategory = categoryRepository.save(category);
 
         // verify data
-        Assertions.assertThat(savedCategory).isNotNull();
-        Assertions.assertThat(savedCategory.getId()).isGreaterThan(0);
-        Assertions.assertThat(savedCategory.getName()).isEqualTo("unit test - category 1");
+        assertThat(savedCategory).isNotNull();
+        assertThat(savedCategory.getId()).isGreaterThan(0);
+        assertThat(savedCategory.getName()).isEqualTo("JUnit test - category 1");
+
+    }
+
+    // JUnit test for update existing category
+    @DisplayName("JUnit test for update existing category")
+    @Test
+    public void whenUpdateCategory_thenReturnCategoryIsUpdated() {
+
+        // save new category into database
+        Category savedCategory = categoryRepository.save(category);
+
+        // get category by id
+        Category existingCategory = categoryRepository.findById(savedCategory.getId()).get();
+
+        // change values of category
+        existingCategory.setName("JUnit test - update category 1");
+        Category updatedCategory = categoryRepository.save(existingCategory);
+
+        // verify data
+        assertThat(updatedCategory.getName()).isEqualTo("JUnit test - update category 1");
+
+    }
+
+    // JUnit test for searching category
+    @DisplayName("JUnit test for searching category")
+    @Test
+    public void whenSearchCategory_thenReturnCategoryList() {
+
+        // save new category1 into database
+        Category savedCategory1 = categoryRepository.save(category);
+
+        // save new category2 into database
+        Category category2 = new Category("JUnit test - category 2", "Active");
+        Category savedCategory2 = categoryRepository.save(category2);
+
+        // search categories
+        List<Category> categoryList = categoryRepository.searchCategories(0, 5, "","");
+
+        // verify data
+        assertThat(categoryList.size()).isEqualTo(2);
+
+    }
+
+    // JUnit test for getting total of categories
+    @DisplayName("JUnit test for getting total of categories")
+    @Test
+    public void whenGetTotalCategories_thenReturnTotalCategories() {
+
+        // save new category1 into database
+        Category savedCategory1 = categoryRepository.save(category);
+
+        // save new category2 into database
+        Category category2 = new Category("JUnit test - category 2", "Active");
+        Category savedCategory2 = categoryRepository.save(category2);
+
+        // get total of categories
+        Long totalOfCategories = categoryRepository.getTotalOfCategories("","");
+
+        // verify data
+        assertThat(totalOfCategories).isEqualTo(2);
 
     }
 }
--- helpdeskspringboot/src/test/java/com/ez/service/CategoryServiceTest.java
+++ helpdeskspringboot/src/test/java/com/ez/service/CategoryServiceTest.java
@@ -2,43 +2,52 @@ package com.ez.service;
 
 import com.ez.entity.Category;
 import com.ez.repository.CategoryRepository;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(MockitoExtension.class)
+@ExtendWith(SpringExtension.class)
 public class CategoryServiceTest {
 
-    @Mock
+    @MockBean
     private CategoryRepository categoryRepository;
 
     @InjectMocks
     private CategoryService categoryService;
 
-//    @BeforeEach
+    //    @BeforeEach
 //    public void setup(){
 //
 //        categoryRepository = Mockito.mock(CategoryRepository.class);
 //        categoryService = new CategoryService(categoryRepository);
 //    }
     @Test
-    public void whenSaveCategory_thenReturnCategory(){
+    public void whenSaveCategory_thenReturnCategory() {
 
 //        Category  category = new Category(1L, "cate1","Active");
-        Category  category = new Category("cate1","Active");
+        Category category = new Category("cate1", "Active");
+        Category savedCategory = new Category(1L, "cate1", "Active");
 
-        BDDMockito.given(categoryRepository.save(category)).willReturn(category);
+        Mockito.when(categoryRepository.save(category)).thenReturn(savedCategory);
+//        BDDMockito.given(categoryRepository.save(category)).willReturn(category);
 //        System.out.println(categoryRepository);
 //        System.out.println(categoryService);
 //
 //        //
-        Category savedCategory = categoryService.createCategory(category);
+        Category savedCategory2 = categoryService.createCategory(category);
+
+        assertThat(savedCategory2).isNotNull();
 //
 //
 //        System.out.println(savedCategory);
